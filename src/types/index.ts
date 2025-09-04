f2c6948a18e7c8a4e54b84c9d416799f081a7592
charly <kouasseu.tamayeu@gmail.com>
2025-09-04 09:07:56 +0100

    creation des type de base de l'application.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,66 @@
-type MaxLength<L extends number, V extends string | number> = V extends string ? V : L
+type Todo = {
+    id: string,
+    title: string,
+    description?: string,
+    category: 'Travaille' | 'Personnel' | 'Ugent' | 'Loisir',
+    priority: 'Haute' | 'Moyenne' | 'Base',
+    status: 'En entente' | 'En cour' | 'Terminer' | 'En retard',
+    dateEnd?: Date,
+    createdAd: Date,
+    updatedAd: Date,
+    completedAd?: Date
+}
 
-const t: MaxLength<6, 'dddd'> = '6'
+type Coordinates = {
+    lat: number,
+    lon: number,
+}
 
-console.log(t)
+type Location = {
+    name: string, // nom de la ville
+    contry: string, // code pays
+    coordinate: Coordinates
+}
+
+type CurrentWeather = {
+    temperature: number, // temperenture actuelle (C)
+    feelsLike: number, // temperature recentire
+    humidity: number, // %
+    pressure: number, // pression (hpa)
+    windSpeed: number, // vitesse du vent (km/h)
+    description: string,
+    icon: string,
+    timestamp: Date
+}
+
+type Forecast = {
+    date: Date,
+    tempMin: number, // temperature minimal
+    tempMax: number, // temperature maximal
+    description: string,
+    icon: string
+}
+
+type Weather = {
+    location: Location,
+    current: CurrentWeather,
+    forecast: Forecast[]
+}
+
+type SettingsApp = {
+    theme: 'light' | 'dark' | 'system',
+    defaultLocation: string,
+    notification: boolean
+}
+
+type WeatherCache = {
+    data: Weather,
+    timestamp: Date,
+    ttl: number
+}
+
+type LocalStorage = {
+    todos: Todo[],
+    settings: SettingsApp,
+    weather: WeatherCache
+}
